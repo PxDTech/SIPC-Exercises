98aec72b9cb640503b086388cbdd275ec5d38072
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-03-24 16:53:29 -0400

    Add files via upload

--- SIPC_1.3.rkt
+++ SIPC_1.3.rkt
@@ -2,7 +2,9 @@
 
 (define (square x) (* x x))
 (define (sum-of-squares-two-largest-numbers x y z)
-  (cond ((and (> x y) (> y z)) (+ (square x) (square y))
-      (and (> y z) (> z x)) (+ (square y) (square z)))))
+  (cond
+   [(and (< x y) (< x z)) (+ (square y) (square z))]
+   [(and (< y z) (< y x)) (+ (square x) (square z))]
+   [else (+ (square x) (square y))]))
 
 (sum-of-squares-two-largest-numbers 5 4 3)
